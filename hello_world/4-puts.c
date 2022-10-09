54cfc88bfa17a0ed292c58c89672ba508229b54a
root <root@DESKTOP-DGLJVAQ.localdomain>
2022-10-09 09:12:48 -0500

    Script 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints only a text
+ * Description: This function does to print the text
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
